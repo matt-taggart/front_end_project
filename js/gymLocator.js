0642a7b81ca5901a420a94e236098c36382216cb
Neelesh <ntekal@gmail.com>
2015-12-19 08:07:54 -0500

    google maps dev resources, going to tinker some more

--- js/gymLocator.js
+++ js/gymLocator.js
@@ -1,37 +1,70 @@
 $(document).ready(function() {
-  $(".btn-default").on("click", function(e){
-    e.preventDefault();
-    var userLocation = $("#userLocation").val();
-    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
-    googleApiUrl += "key=AIzaSyB_9FtkYT4VyHxNc8W3e9WA2-zW7yVHgYA";
-    googleApiUrl += "&address=" + userLocation;
+  // $(".btn-default").on("click", function(e){
+  //   e.preventDefault();
+  //   var userLocation = $("#userLocation").val();
+  //   var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+  //   googleApiUrl += "key=AIzaSyB_9FtkYT4VyHxNc8W3e9WA2-zW7yVHgYA";
+  //   googleApiUrl += "&address=" + userLocation;
 
-    $.ajax({
-      type: "GET",
-      url: googleApiUrl,
-      success: googleApiSuccessHandler
+  //   $.ajax({
+  //     type: "GET",
+  //     url: googleApiUrl,
+  //     success: googleApiSuccessHandler
+  //   });
+
+  // });
+
+  // function googleApiSuccessHandler(response) {
+
+  //   var geoLocation = response.results[0].geometry.location;
+  //   console.log(userLocation);
+  // }
+
+  var map;
+  var infowindow;
+
+  function initMap() {
+    var pyrmont = {
+      lat: -33.867,
+      lng: 151.195
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
     });
 
-  });
+    infowindow = new google.maps.InfoWindow();
 
-  function googleApiSuccessHandler(response) {
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      types: ['store']
+    }, callback);
+  }
 
-    var geoLocation = response.results[0].geometry.location;
-    var flickrApiUrl = "https://api.flickr.com/services/rest/?";
-    var flickrApiParams = {
-      api_key: "5d0b99b598780adb1ce7f682110a03e6",
-      method: "flickr.photos.search",
-      format: "json",
-      nojsoncallback: 1,
-      lat: geoLocation.lat,
-      lon: geoLocation.lng
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
     }
-    
-    $.ajax({
-      type: "GET",
-      url: flickrApiUrl + $.param(flickrApiParams),
-      success: flickrSuccessHandler
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
     });
   }
+
+
 
 });
