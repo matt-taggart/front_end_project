642d3001838d888988fcc0ee95881bf700766dbf
matt-taggart <mtaggart89@gmail.com>
2015-12-20 16:11:30 -0500

    changed parameter to return relative results and got append table functioning to work

--- js/nutrition-api.js
+++ js/nutrition-api.js
@@ -1,19 +1,21 @@
+var apiKey = "rHkKtH6RMPiYlFkjl3jBGWpfcEJB3ZMqkyZmAxHK";
+
 $(document).ready(function() {
 
   $(".food-search").on("click", function(e) {
 
-    var search = $("input:text").val();
-    var apiKey = "rHkKtH6RMPiYlFkjl3jBGWpfcEJB3ZMqkyZmAxHK";
-
-    $("tbody").empty();
+    var search = $("input:text").val();    
 
     e.preventDefault();
 
+    $("thead").empty();
+    $("tbody").empty();
+
     $.ajax({
     type: "GET",
-    url: "http://api.nal.usda.gov/ndb/search/?format=json&q=" + search + "&sort=n&max=25&offset=0&api_key=" + apiKey,
+    url: "http://api.nal.usda.gov/ndb/search/?format=json&q=" + search + "&sort=r&max=25&offset=0&api_key=" + apiKey,
     success: function(data) {
-        var foodList = buildTable(data);
+        var foodList = buildTable(data);           
     },
     error: function(jqXHR, textstatus, errorThrown) {
       console.log(jqXHR);
@@ -23,22 +25,47 @@ $(document).ready(function() {
 
     });
 
+  });
+
     function buildTable(foodData) {
       var itemList = foodData.list.item;
-      var foodGroup;
-      var foodName;
-      var tr;
-
+      var foodGroup, foodName, newDiv, createButton, ndbNumber, createTable, tableHead, categoryHeading, nameHeading, tr;
+      $("table").addClass("bordered");
+      categoryHeading = $("<th>").html("Category");
+      nameHeading = $("<th>").html("Name");
+      $("thead").append(categoryHeading).append(nameHeading);       
       for (var i = 0; i < itemList.length; i++) {
         foodGroup = $("<td>").html(itemList[i].group);
         foodName = $("<td>").html(itemList[i].name);
-        tr = $("<tr>").append(foodGroup).append(foodName);
+        ndbNumber = itemList[i].ndbno;
+        newDiv = $("<td>");
+        createButton = $("<a>")
+                        .addClass("waves-effect waves-light btn cyan nutrition modal-trigger")
+                        .attr("href", "#nutrition-facts")
+                        .html("Nutrition Facts")
+                        .attr('data-ndbnum', ndbNumber);
+        addButton = newDiv.append(createButton);
+        tr = $("<tr>").append(foodGroup).append(foodName).append(addButton);
         $("tbody").append(tr);
       }
-
     }
 
-  });
+    $(document).on('click', '.nutrition', function(){
+      var ndbNumber = $(this).attr('data-ndbnum');
+      $.ajax({
+        type: "GET",
+        url: "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=" + apiKey + "&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=" + ndbNumber,
+        success: function(data) {
+          $(".modal-trigger").leanModal();
+
+        },
+        error: function(jqXHR, textstatus, errorThrown) {
+          console.log(jqXHR);
+          console.log(textstatus);
+          console.log(errorThrown);
+        }
+      });
+    });
 
 
 
