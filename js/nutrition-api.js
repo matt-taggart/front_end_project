236853f74132f802c0f91e4aa7870614c7352aca
matt-taggart <mtaggart89@gmail.com>
2015-12-22 02:37:06 -0500

    all nutrition facts now append to modal.  enter keystroke event listener added to submit value in input box

--- js/nutrition-api.js
+++ js/nutrition-api.js
@@ -2,7 +2,7 @@ var apiKey = "rHkKtH6RMPiYlFkjl3jBGWpfcEJB3ZMqkyZmAxHK";
 
 $(document).ready(function() {
 
-  $(".food-search").on("click", function(e) {
+  $("#food-search").on("click", function(e) {
 
     var search = $("input:text").val();    
 
@@ -49,23 +49,48 @@ $(document).ready(function() {
         $("tbody").append(tr);
       }
     }
-
+    //Saturated fat 606, Trans fat 605, Cholesterol 601, Energy 268, Sodium 307, Fiber 291, Protein 203
     $(document).on('click', '.nutrition', function(e){
       var ndbNumber = $(this).attr('data-ndbnum');
-      var serving, tableHeading;      
+      var serving, tableHeading, dividerA, dividerB, amountPerServing, calories, totalFat, saturatedFat, transFat, cholesterol, sodium, carbs, fiber, sugar, protein;      
       e.preventDefault();
       $(".modal-content").empty();
       $("#nutrition-facts").openModal();
       $.ajax({
         type: "GET",
-        url: "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=" + apiKey + "&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=" + ndbNumber,
+        url: "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=" + apiKey + "&nutrients=205&nutrients=204&nutrients=208&nutrients=269&nutrients=606&nutrients=605&nutrients=601&nutrients=268&nutrients=307&nutrients=291&nutrients=203&ndbno=" + ndbNumber,
         success: function(data) {
           for (var i = 0; i < data.report.foods.length; i++) {
             tableHeading = $("<h4>").html("Nutrition Facts");
             $(".modal-content").append(tableHeading);
-            serving = $("<p>").html("Serving Size " + data.report.foods[i].measure);
+            serving = $("<p>").addClass("serving-size").html("Serving Size " + data.report.foods[i].measure);
             $(".modal-content").append(serving);
-            $("<p>").addClass("divider");
+            dividerA = $("<p>").addClass("dividerA");
+            $(".modal-content").append(dividerA);
+            amountPerServing = $("<p>").addClass("amount-per-serving").addClass("small-divider").html("<strong>Amount Per Serving</strong>");
+            $(".modal-content").append(amountPerServing);
+            calories = $("<p>").addClass("calories").html("<strong>Calories </strong>" + data.report.foods[i].nutrients[9].value);
+            $(".modal-content").append(calories);
+            dividerB = $("<p>").addClass("dividerB");
+            $(".modal-content").append(dividerB);
+            totalFat = $("<p>").addClass("total-fat").addClass("small-divider").html("<strong>Total Fat </strong>" + round(data.report.foods[i].nutrients[2].value) + data.report.foods[i].nutrients[1].unit);
+            $(".modal-content").append(totalFat);
+            saturatedFat = $("<p>").addClass("saturated-fat").addClass("small-divider").html("Saturated Fat " + round(data.report.foods[i].nutrients[7].value) + data.report.foods[i].nutrients[7].unit);
+            $(".modal-content").append(saturatedFat);
+            transFat = $("<p>").addClass("trans-fat").addClass("small-divider").html("Trans Fat " + round(data.report.foods[i].nutrients[8].value) + data.report.foods[i].nutrients[8].unit);
+            $(".modal-content").append(transFat);
+            cholesterol = $("<p>").addClass("cholesterol").addClass("small-divider").html("<strong>Cholesterol</strong> " + round(data.report.foods[i].nutrients[0].value) + data.report.foods[i].nutrients[0].unit);
+            $(".modal-content").append(transFat);
+            sodium = $("<p>").addClass("sodium").addClass("small-divider").html("<strong>Sodium</strong> " + round(data.report.foods[i].nutrients[5].value) + data.report.foods[i].nutrients[5].unit);
+            $(".modal-content").append(sodium);
+            carbs = $("<p>").addClass("carbs").addClass("small-divider").html("<strong>Total Carbohydrate</strong> " + round(data.report.foods[i].nutrients[3].value) + data.report.foods[i].nutrients[3].unit);
+            $(".modal-content").append(carbs);
+            fiber = $("<p>").addClass("fiber").addClass("small-divider").html("Dietary Fiber " + round(data.report.foods[i].nutrients[10].value) + data.report.foods[i].nutrients[10].unit);
+            $(".modal-content").append(fiber);
+            sugar = $("<p>").addClass("sugar").addClass("small-divider").html("Sugars " + round(data.report.foods[i].nutrients[4].value) + data.report.foods[i].nutrients[4].unit);
+            $(".modal-content").append(sugar);
+            protein = $("<p>").addClass("protein").html("<strong>Protein</strong> " + round(data.report.foods[i].nutrients[1].value) + data.report.foods[i].nutrients[1].unit);
+            $(".modal-content").append(protein);   
           }
         },
         error: function(jqXHR, textstatus, errorThrown) {
@@ -76,7 +101,20 @@ $(document).ready(function() {
       });
     });
 
+    $("input:text").keypress(function(e) {
+      if(e.which == 13) {
+        $("#food-search").click();
+      }
+    });
 
+    function round(value) {
+      var number = Math.round(parseInt(value));
+      if(isNaN(number)) {
+        return 0;
+      } else {
+        return number;        
+      }
+    }
 
 });
 
