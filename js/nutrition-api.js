1f8d7cf56d6cc82d9acfa1157714297e53b1c53a
Matthew Christopher Taggart <mtaggart@Matthews-MacBook-Pro.local>
2015-12-23 16:53:58 -0500

    modified side-scrolling table on small screen to a width of 300px and to have word wrap on longer food names

--- js/nutrition-api.js
+++ js/nutrition-api.js
@@ -30,7 +30,6 @@ $(document).ready(function() {
     function buildTable(foodData) {
       var itemList = foodData.list.item;
       var foodGroup, foodName, newDiv, createButton, ndbNumber, createTable, categoryHeading, nameHeading, tr, headTr;
-      
       $("table").addClass("bordered centered bg-white responsive-table");
       categoryHeading = $("<th>").html("Category");
       nameHeading = $("<th>").html("Name");
@@ -69,27 +68,27 @@ $(document).ready(function() {
             $(".modal-content").append(serving);
             dividerA = $("<p>").addClass("dividerA");
             $(".modal-content").append(dividerA);
-            amountPerServing = $("<p>").addClass("amount-per-serving").addClass("small-divider").html("<strong>Amount Per Serving</strong>");
+            amountPerServing = $("<p>").addClass("amount-per-serving small-divider").html("<strong>Amount Per Serving</strong>");
             $(".modal-content").append(amountPerServing);
             calories = $("<p>").addClass("calories").html("<strong>Calories </strong>" + data.report.foods[i].nutrients[9].value);
             $(".modal-content").append(calories);
             dividerB = $("<p>").addClass("dividerB");
             $(".modal-content").append(dividerB);
-            totalFat = $("<p>").addClass("total-fat").addClass("small-divider").html("<strong>Total Fat </strong>" + round(data.report.foods[i].nutrients[2].value) + data.report.foods[i].nutrients[1].unit);
+            totalFat = $("<p>").addClass("total-fat small-divider").html("<strong>Total Fat </strong>" + round(data.report.foods[i].nutrients[2].value) + data.report.foods[i].nutrients[1].unit);
             $(".modal-content").append(totalFat);
-            saturatedFat = $("<p>").addClass("saturated-fat").addClass("small-divider").html("Saturated Fat " + round(data.report.foods[i].nutrients[7].value) + data.report.foods[i].nutrients[7].unit);
+            saturatedFat = $("<p>").addClass("saturated-fats small-divider sm-indent").html("Saturated Fat " + round(data.report.foods[i].nutrients[7].value) + data.report.foods[i].nutrients[7].unit);
             $(".modal-content").append(saturatedFat);
-            transFat = $("<p>").addClass("trans-fat").addClass("small-divider").html("Trans Fat " + round(data.report.foods[i].nutrients[8].value) + data.report.foods[i].nutrients[8].unit);
+            transFat = $("<p>").addClass("trans-fat small-divider sm-indent").html("Trans Fat " + round(data.report.foods[i].nutrients[8].value) + data.report.foods[i].nutrients[8].unit);
             $(".modal-content").append(transFat);
-            cholesterol = $("<p>").addClass("cholesterol").addClass("small-divider").html("<strong>Cholesterol</strong> " + round(data.report.foods[i].nutrients[0].value) + data.report.foods[i].nutrients[0].unit);
+            cholesterol = $("<p>").addClass("cholesterol small-divider").html("<strong>Cholesterol</strong> " + round(data.report.foods[i].nutrients[0].value) + data.report.foods[i].nutrients[0].unit);
             $(".modal-content").append(transFat);
-            sodium = $("<p>").addClass("sodium").addClass("small-divider").html("<strong>Sodium</strong> " + round(data.report.foods[i].nutrients[5].value) + data.report.foods[i].nutrients[5].unit);
+            sodium = $("<p>").addClass("sodium small-divider").html("<strong>Sodium</strong> " + round(data.report.foods[i].nutrients[5].value) + data.report.foods[i].nutrients[5].unit);
             $(".modal-content").append(sodium);
-            carbs = $("<p>").addClass("carbs").addClass("small-divider").html("<strong>Total Carbohydrate</strong> " + round(data.report.foods[i].nutrients[3].value) + data.report.foods[i].nutrients[3].unit);
+            carbs = $("<p>").addClass("carbs small-divider").html("<strong>Total Carbohydrate</strong> " + round(data.report.foods[i].nutrients[3].value) + data.report.foods[i].nutrients[3].unit);
             $(".modal-content").append(carbs);
-            fiber = $("<p>").addClass("fiber").addClass("small-divider").html("Dietary Fiber " + round(data.report.foods[i].nutrients[10].value) + data.report.foods[i].nutrients[10].unit);
+            fiber = $("<p>").addClass("fiber small-divider sm-indent").html("Dietary Fiber " + round(data.report.foods[i].nutrients[10].value) + data.report.foods[i].nutrients[10].unit);
             $(".modal-content").append(fiber);
-            sugar = $("<p>").addClass("sugar").addClass("small-divider").html("Sugars " + round(data.report.foods[i].nutrients[4].value) + data.report.foods[i].nutrients[4].unit);
+            sugar = $("<p>").addClass("sugar small-divider sm-indent").html("Sugars " + round(data.report.foods[i].nutrients[4].value) + data.report.foods[i].nutrients[4].unit);
             $(".modal-content").append(sugar);
             protein = $("<p>").addClass("protein").html("<strong>Protein</strong> " + round(data.report.foods[i].nutrients[1].value) + data.report.foods[i].nutrients[1].unit);
             $(".modal-content").append(protein);
