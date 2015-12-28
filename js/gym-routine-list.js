9261ed4e2a5f5d978f9ccfeb2a7b12c9fc1dc5ee
unknown <jeffreyhshapiro@gmail.com>
2015-12-28 01:36:19 -0500

    created semi working function that extracts JSON information and puts it into modals

--- js/gym-routine-list.js
+++ js/gym-routine-list.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
 
-  var moreInfoModalGenerator = getIdMoreInfo();
+
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      });
 
   function getIdMoreInfo(){
     $(".more-info").on("click",function(){
       var exerciseId = $(this).attr("id");
       console.log(exerciseId);
-      $('.modal-trigger').leanModal({
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        });
-      $(workoutInfo.id).each(), function(){
-        console.log(workoutInfo.id);
-      }
-      
-
+      modalAppendExerciseInfo(exerciseId);
     });
   };
 
+  function modalAppendExerciseInfo(exerciseId){
+    var exerciseTitle = workoutInfo[exerciseId][0].exercise;
+    var exerciseDescription = workoutInfo[exerciseId][1].description
+    var exerciseEqupiment = workoutInfo[exerciseId][1].equipment
+    $("#exercise-title").append(exerciseTitle);
+    $("#exercise-description").append(exerciseDescription);
+    modalReset();
+  }
+
+  function modalReset(){
+    $("#modal-more-info").on("complete",function(){
+      $("#exercise-title").html('');
+    })
+  }
+
+  getIdMoreInfo();
 });
