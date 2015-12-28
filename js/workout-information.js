f7984ea3e4682eb9950b184e8bed710ccff91932
unknown <jeffreyhshapiro@gmail.com>
2015-12-28 12:50:56 -0500

    created fully functional dynamic modals based on button click

--- js/workout-information.js
+++ js/workout-information.js
@@ -7,7 +7,7 @@ var workoutInfo = {
             "description": "Position barbell overhead with narrow overhand grip. Lower forearm behind upper arm with elbows remaining overhead. Extend forearm overhead. Lower and repeat."
         },
         {
-            "equipment": "Barbell"
+            "equipment": "Equipment: Barbell"
         },
         {
             "muscles-front": ""
@@ -16,18 +16,18 @@ var workoutInfo = {
             "muscles-back": ""
         },
         {
-            "workout-video-url": "https://www.youtube.com/watch?v=a2TkTaXagRQ"
+            "workoutVideoUrl": '<iframe width="420" height="345" src="https://www.youtube.com/embed/a2TkTaXagRQ"></iframe>'
         }
     ],
     "bench-press-narrow-grip": [
         {
-            "exercise": "bench-press-narrow-grip"
+            "exercise": "Bench Press Narrow Grip"
         },
         {
             "description": "Lay down on a bench, the bar is directly over your eyes, the knees form a slight angle and the feet are firmly on the ground. Hold the bar with a narrow grip (around 20cm.). Lead the weight slowly down till the arms are parallel to the floor (elbow: right angle), press then the bar up. When bringing the bar down, don't let it down on your nipples as with the regular bench pressing, but somewhat lower."
         },
         {
-            "equipment": "Barbell, bench"
+            "equipment": "Equipment: Barbell, bench"
         },
         {
             "muscles-front": ""
@@ -36,7 +36,67 @@ var workoutInfo = {
             "muscles-back": ""
         },
         {
-            "workout-video-url": ""
+            "workoutVideoUrl": '<iframe width="420" height="345" src="https://www.youtube.com/embed/Ruy94BZ6FmE"></iframe>'
+        }
+    ],
+    "biceps-curl-with-cable": [
+        {
+            "exercise": "Biceps Curl With Cable"
+        },
+        {
+            "description": "Stand around 30 - 40cm away from the cable, the feet are firmly on the floor. Take the bar and lift the weight with a fast movements. Lower the weight as with the dumbbell curls slowly and controlled. <strong>NOTE: Do not move elbows during exercise!</strong>"
+        },
+        {
+            "equipment": "Equipment: Weight machine with curling bar"
+        },
+        {
+            "muscles-front": ""
+        },
+        {
+            "muscles-back": ""
+        },
+        {
+            "workoutVideoUrl": '<iframe width="420" height="345" src="https://www.youtube.com/embed/kyyP5l8noSY"></iframe>'
+        }
+    ],
+    "biceps-curl-with-barbell": [
+        {
+            "exercise": "Biceps Curl With Barbell"
+        },
+        {
+            "description": "Hold the Barbell shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.<br><br>Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows)."
+        },
+        {
+            "equipment": "Equipment: Barbell"
+        },
+        {
+            "muscles-front": ""
+        },
+        {
+            "muscles-back": ""
+        },
+        {
+            "workoutVideoUrl": '<iframe width="420" height="345" src="https://www.youtube.com/embed/uO_CNYidOw0"></iframe>'
+        }
+    ],
+    "biceps-curl-with-dumbbell": [
+        {
+            "exercise": "Biceps Curl With Dumbbell"
+        },
+        {
+            "description": "Hold two barbells, the arms are streched, the hands are on your side, the palms face inwards. Bend the arms and bring the weight with a fast movement up. At the same time, rotate your arms by 90 degrees at the very beginning of the movement. At the highest point, rotate a little the weights further outwards. Without a pause, bring them down, slowly.<br><br>Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows)."
+        },
+        {
+            "equipment": "Equipment: Dumbbell"
+        },
+        {
+            "muscles-front": ""
+        },
+        {
+            "muscles-back": ""
+        },
+        {
+            "workoutVideoUrl": '<iframe width="420" height="345" src="https://www.youtube.com/embed/av7-8igSXTs"></iframe>'
         }
     ]
 }
