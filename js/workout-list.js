9f65e469abc9a39bba2060f131fbe3e1e112fbd9
unknown <jeffreyhshapiro@gmail.com>
2016-01-01 22:49:11 -0500

    fully functional workout list to enter workout pending entry restrictions

--- js/workout-list.js
+++ js/workout-list.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
 
+  //Get the name of the workout to append to the workout list
+
   $(".add-workout").on("click", function(){
     var workoutExerciseIdForList = $(this).attr("id");
     console.log(workoutExerciseIdForList);
     workoutListAppend(workoutExerciseIdForList);
   });
 
+  //Append all necessary elements to workout list
 
   function workoutListAppend(workoutExerciseIdForList){
     var workoutName = workoutInfo[workoutExerciseIdForList][0].exercise;
     var workoutNameLink = $("<a>").attr("href","#modal-more-info").attr("id",workoutExerciseIdForList).addClass("modal-trigger").append(workoutName);
     var workoutAppend = $("<td>").append(workoutNameLink);
-    var repAppend = $("<td>").append('<div class="input-field"><input id="reps" type="text" class="validate"><label for="reps"></label></div>');
-    var setAppend = $("<td>").append('<div class="input-field"><input id="sets" type="text" class="validate"><label for="sets"></label></div>');
-    var weightAppend = $("<td>").append('<div class="input-field"><input id="weight" type="text" class="validate"><label for="weight"></label></div>');
+    var repAppend = $("<td>").append('<div class="input-field"><input class="formReps" id="reps-'+workoutExerciseIdForList+'" type="text" class="validate"><label for="reps-'+workoutExerciseIdForList+'"></label></div>');
+    var setAppend = $("<td>").append('<div class="input-field"><input id="sets-'+workoutExerciseIdForList+'" type="text" class="validate"><label for="sets-'+workoutExerciseIdForList+'"></label></div>');
+    var weightAppend = $("<td>").append('<div class="input-field"><input id="weight-'+workoutExerciseIdForList+'" type="text" class="validate"><label for="weight-'+workoutExerciseIdForList+'"></label></div>');
     var completeButtonAppend = $("<td>").append('<input type="checkbox" class="workoutCheckbox" id='+workoutExerciseIdForList+'-checkbox /><label for='+workoutExerciseIdForList+'-checkbox></label>');
     var removeWorkoutButton = $("<td>").append('<a class="waves-effect waves-light red btn">X</a>');
     var workoutAppendToTr = $("<tr>").append(workoutAppend)
@@ -24,15 +27,14 @@ $(document).ready(function(){
                                      .append(removeWorkoutButton);
     $("#workout-append").append(workoutAppendToTr);
 
-
+    enterToSubmitReps(workoutExerciseIdForList);
+    enterToSubmitSets(workoutExerciseIdForList);
+    enterToSubmitWeight(workoutExerciseIdForList);
     checkboxHandler();
     removeWorkoutHandler();
-
-    $(workoutAppend).on("click", function(){
-      modalAppendExerciseInfo(workoutExerciseIdForList);
-    });
-
   };
+
+  //Upon clicking checkbox, checkbox is disabled and the remove button is deleted
 
   function checkboxHandler(){
     $(".workoutCheckbox").on("click", function(){
@@ -41,6 +43,8 @@ $(document).ready(function(){
       completeMessage();
       });
     };
+
+  //An engaging message is displayed to the user
 
   function completeMessage(){
     var congratsMessage = ["Your power level is truly over 9000", "You better be sure to bring your tickets to the gun show", "What has two thumbs and huge biceps? This guy (or girl 8-) )", "You have come one stepped closer to getting yoked. Make sure to eat something healthy!", "You have successfully picked things up AND put them down"];
@@ -54,29 +58,37 @@ $(document).ready(function(){
     });
   };
 
-  });
-
-
-
-
-
-
-  /*function modalReset(){
-    $("#exercise-title").empty();
-    $("#exercise-description").empty();
-    $("#exercise-equipment").empty();
-    $("#exercise-video").empty();
+  function enterToSubmitReps(workoutExerciseIdForList){
+    $("#reps-"+workoutExerciseIdForList).on("keydown", function(e) {
+      if (e.keyCode == 13) {
+        var repAmount = $(this).val();
+        console.log(repAmount);
+        $(this).closest('td').append(repAmount);
+        $(this).closest('div').remove();
+      };
+    });
   };
 
-  function modalAppendExerciseInfo(workoutExerciseIdForList){
-    var exerciseTitle = workoutInfo[workoutExerciseIdForList][0].exercise;
-    var exerciseDescription = workoutInfo[workoutExerciseIdForList][1].description;
-    var exerciseEquipment = workoutInfo[workoutExerciseIdForList][2].equipment;
-    var exerciseVideo = workoutInfo[workoutExerciseIdForList][5].workoutVideoUrl;
-    $("#exercise-title").append(exerciseTitle);
-    $("#exercise-description").append(exerciseDescription);
-    $("#exercise-equipment").append(exerciseEquipment);
-    $("#exercise-video").append(exerciseVideo);
-  }*/
+  function enterToSubmitSets(workoutExerciseIdForList){
+    $("#sets-"+workoutExerciseIdForList).on("keydown", function(e) {
+      if (e.keyCode == 13) {
+        var setAmount = $(this).val();
+        console.log(setAmount);
+        $(this).closest('td').append(setAmount);
+        $(this).closest('div').remove();
+      };
+    });
+  };
 
-//});
+  function enterToSubmitWeight(workoutExerciseIdForList){
+    $("#weight-"+workoutExerciseIdForList).on("keydown", function(e) {
+      if (e.keyCode == 13) {
+        var weightAmount = $(this).val();
+        console.log(weightAmount);
+        $(this).closest('td').append(weightAmount);
+        $(this).closest('div').remove();
+      };
+    });
+  };
+
+});
