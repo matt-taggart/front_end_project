83b4b6d2a359584886696b060f6a83656ee9c35c
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-03 10:20:38 -0500

    Fixed modal close button and added cyan color to title and icons in each section

--- js/animations.js
+++ js/animations.js
@@ -10,17 +10,18 @@ $(document).ready(function(){
     }
   });
 
-   $(window).scroll( function(){
+
+  $(window).scroll( function(){
 
     /* Check the location of each desired element */
     $('.hideme').each( function(i){
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-        /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > bottom_of_object ){
-          $(this).addClass("animated slideInLeft").animate({'opacity':'1'},2000);
-        }
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+        $(this).addClass("animated slideInLeft").animate({'opacity':'1'},2000);
+      }
     }); 
   });
 });
