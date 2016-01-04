8a173c4a0b8db44c8d1920363d5b61b8a6b83bc3
Matthew Christopher Taggart <mtaggart@Matthews-MacBook-Pro.local>
2016-01-04 17:20:48 -0500

    Added comments to nutrition_api.js file and indentation to make the code more readable

--- js/nutrition-api.js
+++ js/nutrition-api.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
         "api_key": apiKey
       }
 
+      //Ajax call to retrieve food info based on user input
       $.ajax({
         type: "GET",
         url: "https://api.nal.usda.gov/ndb/search/?" + $.param(foodSearchParams),
@@ -32,20 +33,26 @@ $(document).ready(function() {
 
   });
 
+    //Build table rows for each food item and append to html
     function buildTable(foodData) {
       var itemList = foodData.list.item;
       var foodGroup, foodName, checkbox, input, label, appendCheckbox, newDiv, createButton, addButton, floatingButton, ndbNumber, createTable, categoryHeading, nameHeading, tr, headTr;
       $("table").addClass("bordered centered bg-white responsive-table animated fadeIn");
       categoryHeading = $("<th>").html("Category");
       nameHeading = $("<th>").html("Name");
-      headTr = $("<tr>").append(categoryHeading).append(nameHeading).append("<th></th>");
-      $("thead").addClass("centered").append(headTr);       
+      headTr = $("<tr>")
+                  .append(categoryHeading)
+                  .append(nameHeading)
+                  .append("<th></th>");
+      $("thead")
+          .addClass("centered")
+          .append(headTr);       
       for (var i = 0; i < itemList.length; i++) {
         foodGroup = $("<td>").html(itemList[i].group);
         foodName = $("<td>").html(itemList[i].name);
-        // input = $("<input>").attr("type", "checkbox").attr("id", "myCheckbox").addClass("filled-in");
-        // label = $("<label>").attr("for", "myCheckbox");
-        checkbox = $("<p>").append(input).append(label);
+        checkbox = $("<p>")
+                      .append(input)
+                      .append(label);
         ndbNumber = itemList[i].ndbno;
         newDiv = $("<td>");
         createButton = $("<a>")
@@ -54,11 +61,17 @@ $(document).ready(function() {
                         .html("Nutrition Facts")
                         .attr('data-ndbnum', ndbNumber);
         addButton = newDiv.append(createButton);
-        tr = $("<tr>").addClass("animated fadeIn").append(foodGroup).append(foodName).append(addButton);
+        tr = $("<tr>")
+                .addClass("animated fadeIn")
+                .append(foodGroup)
+                .append(foodName)
+                .append(addButton);
         $("tbody").append(tr);
       }
     }
-    //Saturated fat 606, Trans fat 605, Cholesterol 601, Energy 268, Sodium 307, Fiber 291, Protein 203
+
+    //When 'nutrition facts' button is clicked, return all relevant nutrition data for this particular food choice and append to modal.  
+    //Nutrient Parameters = Saturated fat 606, Trans fat 605, Cholesterol 601, Energy 268, Sodium 307, Fiber 291, Protein 203
     $(document).on('click', '.nutrition', function(e){
       e.preventDefault();
       var ndbNumber = $(this).attr('data-ndbnum');
@@ -66,6 +79,8 @@ $(document).ready(function() {
       var addCalories, addFat, addSatFat, addTransFat, addCholesterol, addSodium, addCarbs, addFiber, addSugar, addProtein; 
       $(".modal-content").empty();
       $("#nutrition-facts").openModal();
+
+      //Ajax call to get nutrient info for food items based on individual gov't ndbnum
       $.ajax({
         type: "GET",
         url: "https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=" + apiKey + "&nutrients=205&nutrients=204&nutrients=208&nutrients=269&nutrients=606&nutrients=605&nutrients=601&nutrients=268&nutrients=307&nutrients=291&nutrients=203&ndbno=" + ndbNumber,
@@ -84,33 +99,58 @@ $(document).ready(function() {
             measure = data.report.foods[i].measure;        
             tableHeading = $("<h4>").html("Nutrition Facts");
             appendToModal(tableHeading);
-            serving = $("<p>").addClass("serving-size").html("Serving Size " + parseUnitOfMeasure(measure));
+            serving = $("<p>")
+                        .addClass("serving-size")
+                        .html("Serving Size " + parseUnitOfMeasure(measure));
             appendToModal(serving);
             dividerA = $("<p>").addClass("dividerA");
             appendToModal(dividerA);
-            amountPerServing = $("<p>").addClass("amount-per-serving small-divider").html("<strong>Amount Per Serving</strong>");
+            amountPerServing = $("<p>")
+                                .addClass("amount-per-serving small-divider")
+                                .html("<strong>Amount Per Serving</strong>");
             appendToModal(amountPerServing);
-            addCalories = $("<p>").addClass("calories").html("<strong>Calories </strong>" + calories.value + "<span class='lg-indent'>Calories from Fat " + round(totalFat.value*9) + "</span>");
+            addCalories = $("<p>")
+                            .addClass("calories")
+                            .html("<strong>Calories </strong>" + calories.value + "<span class='lg-indent'>Calories from Fat " + round(totalFat.value*9) + "</span>");
             appendToModal(addCalories);
             dividerB = $("<p>").addClass("dividerB");
             appendToModal(dividerB);
-            addFat = $("<p>").addClass("total-fat small-divider").html("<strong>Total Fat </strong>" + roundHalf(totalFat.value) + totalFat.unit);
+            addFat = $("<p>")
+                      .addClass("total-fat small-divider")
+                      .html("<strong>Total Fat </strong>" + roundHalf(totalFat.value) + totalFat.unit);
             appendToModal(addFat);
-            addSatFat = $("<p>").addClass("saturated-fats small-divider sm-indent").html("Saturated Fat " + roundHalf(saturatedFat.value) + saturatedFat.unit);
+            addSatFat = $("<p>")
+                          .addClass("saturated-fats small-divider sm-indent")
+                          .html("Saturated Fat " + roundHalf(saturatedFat.value) + saturatedFat.unit);
             appendToModal(addSatFat);
-            addTransFat = $("<p>").addClass("trans-fat small-divider sm-indent").html("Trans Fat " + roundHalf(transFat.value) + transFat.unit);
+            addTransFat = $("<p>")
+                            .addClass("trans-fat small-divider sm-indent")
+                            .html("Trans Fat " + roundHalf(transFat.value) + transFat.unit);
             appendToModal(addTransFat);
-            addCholesterol = $("<p>").addClass("cholesterol small-divider").html("<strong>Cholesterol</strong> " + round(cholesterol) + cholesterol.unit);
+            addCholesterol = $("<p>")
+                              .addClass("cholesterol small-divider")
+                              .html("<strong>Cholesterol</strong> " + round(cholesterol) + cholesterol.unit);
             appendToModal(addCholesterol);
-            addSodium = $("<p>").addClass("sodium small-divider").html("<strong>Sodium</strong> " + round(sodium.value) + sodium.unit);
+            addSodium = $("<p>")
+                          .addClass("sodium small-divider")
+                          .html("<strong>Sodium</strong> " + round(sodium.value) + sodium.unit);
             appendToModal(addSodium);
-            addCarbs = $("<p>").addClass("carbs small-divider").html("<strong>Total Carbohydrate</strong> " + round(carbs.value) + carbs.unit);
+            addCarbs = $("<p>")
+                        .addClass("carbs small-divider")
+                        .html("<strong>Total Carbohydrate</strong> " + round(carbs.value) + carbs.unit);
             appendToModal(addCarbs);
-            addFiber = $("<p>").addClass("fiber small-divider sm-indent").html("Dietary Fiber " + roundHalf(fiber.value) +fiber.unit);
+
+            addFiber = $("<p>")
+                          .addClass("fiber small-divider sm-indent")
+                          .html("Dietary Fiber " + roundHalf(fiber.value) +fiber.unit);
             appendToModal(addFiber);
-            addSugar = $("<p>").addClass("sugar small-divider sm-indent").html("Sugars " + round(sugar.value) + sugar.unit);
+            addSugar = $("<p>")
+                        .addClass("sugar small-divider sm-indent")
+                        .html("Sugars " + round(sugar.value) + sugar.unit);
             appendToModal(addSugar);
-            addProtein = $("<p>").addClass("protein").html("<strong>Protein</strong> " + round(protein.value) + protein.unit);
+            addProtein = $("<p>")
+                          .addClass("protein")
+                          .html("<strong>Protein</strong> " + round(protein.value) + protein.unit);
             appendToModal(addProtein);
             dividerC = $("<p>").addClass("dividerC");
             appendToModal(dividerC);
@@ -124,12 +164,14 @@ $(document).ready(function() {
       });
     });
 
+    //Execute food search function when user presses enter on keyboard 
     $("input:text").keypress(function(e) {
       if(e.which == 13) {
         $("#food-search").click();
       }
     });
 
+    //Round number to nearest integer & convert non-integer string values to zero
     function round(value) {
       var number = parseFloat(value) ;
       if(isNaN(number)) {
@@ -139,6 +181,7 @@ $(document).ready(function() {
       }
     }
 
+    //Round number to nearest .5 & convert non-integer string values to zero
     function roundHalf(value) {
       var number = parseFloat(value) ;
       if(isNaN(number)) {
@@ -148,12 +191,15 @@ $(document).ready(function() {
       }
     }
 
+    //Find and replace serving size & round to nearest 0.5
     function parseUnitOfMeasure(value) {
       var unitOfMeasure = /\d+\.+\d/.exec(value);
       var roundedUom = roundHalf(unitOfMeasure);
       return value.replace(unitOfMeasure, roundedUom);
     }
 
+
+    //Append each row containing nutrient data to modal
     function appendToModal(nutrient) {
       $(".modal-content").append(nutrient);
     }
