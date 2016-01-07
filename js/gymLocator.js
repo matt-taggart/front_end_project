e192e01b2b3e8c3922005fee9ab8c871a50035de
Neelesh <ntekal@gmail.com>
2016-01-06 19:35:03 -0500

    responsive mapgit add .git add .

--- js/gymLocator.js
+++ js/gymLocator.js
@@ -7,58 +7,11 @@ var MARKER_PATH = 'https://maps.gstatic.com/intl/en_us/mapfiles/marker_green';
 var hostnameRegexp = new RegExp('^https?://.+?/');
 
 var countries = {
-  'au': {
-    center: {lat: -25.3, lng: 133.8},
-    zoom: 4
-  },
-  'br': {
-    center: {lat: -14.2, lng: -51.9},
-    zoom: 3
-  },
-  'ca': {
-    center: {lat: 62, lng: -110.0},
-    zoom: 3
-  },
-  'fr': {
-    center: {lat: 46.2, lng: 2.2},
-    zoom: 5
-  },
-  'de': {
-    center: {lat: 51.2, lng: 10.4},
-    zoom: 5
-  },
-  'mx': {
-    center: {lat: 23.6, lng: -102.5},
-    zoom: 4
-  },
-  'nz': {
-    center: {lat: -40.9, lng: 174.9},
-    zoom: 5
-  },
-  'it': {
-    center: {lat: 41.9, lng: 12.6},
-    zoom: 5
-  },
-  'za': {
-    center: {lat: -30.6, lng: 22.9},
-    zoom: 5
-  },
-  'es': {
-    center: {lat: 40.5, lng: -3.7},
-    zoom: 5
-  },
-  'pt': {
-    center: {lat: 39.4, lng: -8.2},
-    zoom: 6
-  },
   'us': {
     center: {lat: 37.1, lng: -95.7},
     zoom: 3
-  },
-  'uk': {
-    center: {lat: 54.8, lng: -4.6},
-    zoom: 5
   }
+
 };
 
 function initMap() {
@@ -90,6 +43,9 @@ function initMap() {
   // Add a DOM event listener to react when the user selects a country.
   document.getElementById('country').addEventListener(
       'change', setAutocompleteCountry);
+
+  google.maps.event.addDomListener(window, 'resize', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 // When the user selects a city, get the place details for the city and
@@ -268,5 +224,3 @@ function buildIWContent(place) {
   }
 }
 
-google.maps.event.addDomListener(window, 'resize', initialize);
-google.maps.event.addDomListener(window, 'load', initialize);
